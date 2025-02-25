bff75559e9d488a0a2cec0f7a269a2858e7d1d6b
Negri234279 <rafalejo01@gmail.com>
2025-02-26 00:11:35 +0100

    feat: Mejorar la presentación de información del jugador con soporte para Steam ID y Discord ID

--- src/commands/players.command.ts
+++ src/commands/players.command.ts
@@ -1,4 +1,10 @@
-import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import {
+    APIEmbedField,
+    ChatInputCommandInteraction,
+    EmbedBuilder,
+    SlashCommandBuilder,
+} from 'discord.js'
+
 import { container } from '../configs/container'
 import TYPES from '../configs/types'
 import ControlledError from '../errors/controlled.error'
@@ -24,20 +30,52 @@ const command: Command = {
             throw new ControlledError('No hay jugadores en la lista')
         }
 
-        const promises = players.map(async ({ battlemetrics_id, alias }) => {
+        const promises = players.map(async (playerFound) => {
+            const { battlemetrics_id, alias, steam_id, discord_id } = playerFound
             const { player, server } = await getLastServerPlayed(battlemetrics_id)
 
-            return new EmbedBuilder()
-                .setTitle(`Último servidor jugado de ${player.name} (${player.id}) alias ${alias}`)
-                .setDescription(
-                    `${formatSeconds(server.timePlayed)}\n${formatLastSeen(server.lastSeen)}`
-                )
-                .setColor('#0099ff')
-                .addFields({
+            const title =
+                player.name === alias
+                    ? `Último servidor jugado de ${player.name} (${player.id})`
+                    : `Último servidor jugado de ${player.name} alias ${alias} (${player.id})`
+
+            let description = `${formatSeconds(server.timePlayed)}`
+            description += `\n${formatLastSeen(server.lastSeen)}`
+
+            const fields: APIEmbedField[] = [
+                {
                     name: server.name,
                     value: `Estado: ${player.status}`,
                     inline: true,
+                },
+            ]
+
+            const battlemetricsUrl = `https://www.battlemetrics.com/players/${player.id}`
+
+            let info = `BattlemetricsId: [${player.id}](${battlemetricsUrl})`
+
+            if (steam_id) {
+                const profileurl = `https://steamcommunity.com/profiles/${steam_id}`
+                info += `\nSteamId: [${steam_id}](${profileurl})`
+            }
+
+            if (discord_id) {
+                info += `\nDiscordId: ${discord_id}`
+            }
+
+            if (info) {
+                fields.push({
+                    name: 'Otra información',
+                    value: info,
+                    inline: true,
                 })
+            }
+
+            return new EmbedBuilder()
+                .setTitle(title)
+                .setDescription(description)
+                .setColor('#0099ff')
+                .addFields(...fields)
         })
 
         const embeds = await Promise.all(promises)
